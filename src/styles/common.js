b74ead8520c64e1fac462c58819e3e2040c64aff
Steve <kgogo9@gmail.com>
2023-01-02 17:43:42 +0900

    폰트 적용

--- src/styles/common.js
+++ src/styles/common.js
@@ -2,11 +2,17 @@ import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
+    @font-face {
+        font-family: 'Pretendard-Regular';
+        src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+        font-weight: 400;
+        font-style: normal;
+    }
     ${reset};
     body{
         padding: 0;
         margin: 0;
-        font-family: 'Noto Sans KR', sans-serif;
+        font-family: 'Pretendard-Regular', sans-serif;
     };
     button{
         display: flex;
